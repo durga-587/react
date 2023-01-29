51ad53f45bcbba8bfdbd46cdace4a9f80d10aa1b
durga <durgakakaraparthi7@gmail.com>
2023-01-29 15:35:09 +0530

    first

---
+++ demo/src/heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading(apple){
+    return(
+        <div>
+        <h1> Welcome ...</h1>
+        <p2> { apple.banana}</p2>
+        </div>
+    );
+}
+
+export default Heading;
